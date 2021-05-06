07a437f579b93dd92498796c13cc739415547f14
jomavera <josemanuel.vera.aray@gmail.com>
2021-05-06 09:39:27 -0500

    Completed code for Faust processor of Postgres stream

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -7,7 +7,6 @@ import faust
 logger = logging.getLogger(__name__)
 
 
-# Faust will ingest records from Kafka in this format
 class Station(faust.Record):
     stop_id: int
     direction_id: str
@@ -21,7 +20,6 @@ class Station(faust.Record):
     green: bool
 
 
-# Faust will produce records to Kafka in this format
 class TransformedStation(faust.Record):
     station_id: int
     station_name: str
@@ -29,29 +27,36 @@ class TransformedStation(faust.Record):
     line: str
 
 
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
+
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
 
+topic = app.topic("cta.stations", value_type=Station)
+out_topic = app.topic("stations.table", partitions=1, value_type=TransformedStation)
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+table = app.Table(
+   "TransformedStation",
+   default=int,
+   partitions=1,
+   changelog_topic=out_topic,
+)
+
+@app.agent(topic)
+async def setLine(station_stream):
+    async for station_event in station_stream:
+        if station_event.red == True:
+            line_color = 'red'
+        elif station_event.blue == True:
+            line_color = 'blue'
+        elif station_event.green == True:
+            line_color = 'green'
+        station_event_processed = TransformedStation(
+            station_id = station_event.station_id,
+            station_name = station_event.station_name,
+            order = station_event.order,
+            line = line_color
+        )
+        table[station_event.station_id] = station_event_processed
+        await out_topic.send(key= str(station_event.station_id), value=station_event_processed)
 
 
 if __name__ == "__main__":
