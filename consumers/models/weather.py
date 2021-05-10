0f42c5947b968a32f9ff188d737ad4ba2f4b22a8
jomavera <josemanuel.vera.aray@gmail.com>
2021-05-10 09:55:11 -0500

    Completed Weather consumer

--- consumers/models/weather.py
+++ consumers/models/weather.py
@@ -15,9 +15,8 @@ class Weather:
 
     def process_message(self, message):
         """Handles incoming weather data"""
-        logger.info("weather process_message is incomplete - skipping")
-        #
-        #
-        # TODO: Process incoming weather messages. Set the temperature and status.
-        #
-        #
+
+        value = json.loads(message.value())
+
+        self.temperature = value.get("temperatue")
+        self.status = value.get("status")
